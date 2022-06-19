9c49ba60f04ec72b43f9aad07a463e5b2064f262
Roberto <rob7th@gmail.com>
2022-06-19 14:29:02 -0500

    new user line function

--- service/client-service.js
+++ service/client-service.js
@@ -1,4 +1,5 @@
-console.log("client-service--test"); 
+// VARIABLES ----------->
+console.log("client-service--test");
 const http = new XMLHttpRequest();
 // abrir http (método, url)
 
@@ -9,11 +10,39 @@ UPDATE  - PUT/PATCH
 DELETE  - DELETE
 */
 
+// FUNCION CREAR NUEVA LINEA USUARIO
+const crearNuevaLinea = () => {
+  <tr>
+    <td class="td" data-td>
+      Gabriela
+    </td>
+    <td>gabriela@alura.com</td>
+    <td>
+      <ul class="table__button-control">
+        <li>
+          <a
+            href="../screens/editar_cliente.html"
+            class="simple-button simple-button--edit"
+          >
+            Editar
+          </a>
+        </li>
+        <li>
+          <button class="simple-button simple-button--delete" type="button">
+            Eliminar
+          </button>
+        </li>
+      </ul>
+    </td>
+  </tr>;
+};
+
+// FUNCION GET SEND ------------>
 http.open("GET", "http://localhost:3000/perfil");
 http.send();
 http.onload = () => {
-    const data = http.response;
-    console.log(data);
-} 
+  const data = http.response;
+  console.log(data);
+};
 
 console.log(http);
