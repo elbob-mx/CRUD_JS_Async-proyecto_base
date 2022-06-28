04bcccfc50831850e2efb2a44947f50bf9fed687
elbob-mx <rob7th@gmail.com>
2022-06-28 17:50:04 -0500

    actualizar editar cliente

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -1,2 +1,30 @@
-const url = new URL(window.location);
-const id = url.searchParams.get("id");
+import { clientServices } from "../service/client-service.js";
+
+const formulario = document.querySelector("[data-form]");
+
+const obtenerInformacion = async () => {
+  const url = new URL(window.location);
+  const id = url.searchParams.get("id");
+
+  if (id === null) {
+    window.location.href = "/screens/error.html";
+  };
+
+  const nombre = document.querySelector("[data-nombre]");
+  const email = document.querySelector("[data-email]");
+  const perfil = await clientServices.detalleCliente("id");
+  nombre.value = perfil.nombre;
+  email.value = perfil.email;
+};
+
+obtenerInformacion();
+
+formulario.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const url = new URL(window.location);
+  const id = url.searchParams.get("id");
+
+  const nombre = document.querySelector("[data-nombre]").value;
+  const email = document.querySelector("[data-email]").value;
+  clientServices.actualizarCliente(nombre, email, id).then(() => {window.location.href = "/screens/edicion_concluida.html"});
+});
